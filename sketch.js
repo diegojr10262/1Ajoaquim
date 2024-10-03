bb58686cc3d660e120f33be30e8e034298e59c96
diegojr10262 <junior.dionathan@escola.pr.gov.br>
2024-10-03 11:50:30 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+//açao, guerra, ficção cientifica, terror
+
+// gigantes de aço, 10 
+// circulo e fogo a revolta, 12
+// kingsman, 12
+// até o ultimo homem, 16
+// godzila vs kong, 12
+// padre, 
+
+
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
